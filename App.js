f6d39a12e8acd581f5d86cd08f22d8ac15f9cffa
NhatTien1993 <113179810+NhatTien1993@users.noreply.github.com>
2022-12-27 23:01:40 +0700

    Update App.js

--- App.js
+++ App.js
@@ -2,17 +2,11 @@ import React, { useEffect } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import messaging from '@react-native-firebase/messaging';
 
-import Login2 from './src/Login2_Formik/Login2'
 
 import { Provider } from 'react-redux';
 import { store } from './store'
-import ShoesApp from './src/ShoesApp/ShoesApp';
-import DemoLocalStorage from './src/DemoLocalStorage/DemoLocalStorage';
 import { KEY_SCREEN } from './src/common/Constant';
-import ShowAllShoes from './src/Login2_Formik/HomePage';
-import EditProfilePage from './src/Profile_app/profile/EditProfilePage';
-import DemoNotification from './src/DemoNotification/DemoNotification';
-import DemoGoogleMap from './src/DemoGoogleMaps/DemoGoogleMap';
+
 import HomePage from './src/screen/HomePage/HomePage';
 import { RootStackNavigator } from './src/Routes/RootStackNavigation';
 
@@ -35,31 +29,6 @@ export default function App() {
     <Provider store={store}>
       <NavigationContainer>
       <RootStackNavigator />
-        {/* <Stack.Navigator
-          initialRouteName={KEY_SCREEN.homePage} > */}
-          {/* <Stack.Screen name={KEY_SCREEN.homePage} component={HomePage}
-            options={{ headerShown: false }} />
-          <Stack.Screen name={KEY_SCREEN.homeShoes} component={ShowAllShoes}
-            options={{
-             headerShown:false
-            }} /> */}
-          {/* <Stack.Screen name={KEY_SCREEN.profile} component={EditProfilePage}
-            options={{
-             headerShown:false
-            }} /> */}
-          {/* <Stack.Screen name={'Home_Shoes'} component={ShoesApp}
-            options={{
-             headerShown:false
-            }} /> */}
-          {/* <Stack.Screen name={'Demo_Notification'} component={DemoNotification}
-            options={{
-             headerShown:false
-            }} /> */}
-          {/* <Stack.Screen name={'Demo_GoogleMap'} component={DemoGoogleMap}
-            options={{
-             headerShown:false
-            }} /> */}
-        {/* </Stack.Navigator> */}
       </NavigationContainer>
     </Provider>
   )
