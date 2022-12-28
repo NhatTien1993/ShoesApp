d7bd7971614c404339673acec71336254bcf1d6f
TienNhat <nhatmt@dps.com.vn>
2022-12-28 15:44:07 +0700

    fix store.js

--- store.js
+++ store.js
@@ -7,17 +7,9 @@
 //Redux toolkit
 
 import {configureStore} from '@reduxjs/toolkit'
-import homePageSlice from './src/Login2_Formik/HomePageSlice';
-import loginSlice from './src/Login2_Formik/LoginSlice';
-import profileSlice from './src/Profile_app/profile/redux/ProfileSlice';
-import shoesSlice from './src/ShoesApp/redux/ReduxSlice';
 import reduxShoesApp from './src/redux/ReduxSlice'
 export const store = configureStore({
     reducer: {
-              login2:loginSlice,
-              shoesApp:shoesSlice,
-              homePage: homePageSlice,
-              profile:profileSlice,
               redux:reduxShoesApp
     }
   })
